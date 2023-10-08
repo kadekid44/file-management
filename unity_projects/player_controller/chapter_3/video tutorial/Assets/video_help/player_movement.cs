aa4001d3a41ed76bb7eba7f1c9bc40572dd0e7f3
Kade Gledhill <kadeoaks4@gmail.com>
2023-10-07 19:58:40 -0600

    box vs. capulse

--- unity_projects/player_controller/chapter_3/video tutorial/Assets/video_help/player_movement.cs
+++ unity_projects/player_controller/chapter_3/video tutorial/Assets/video_help/player_movement.cs
@@ -6,9 +6,12 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpForce = 5.0f;
+    public bool isonGround = true;
     private float horizontalInput;
     private float forwardInput;
+
     private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +24,24 @@ public class NewBehaviourScript : MonoBehaviour
         // get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
+
         //move the player forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-            //let the player jump
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            }
+        //let the player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isonGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isonGround = false;
+        }
     }
+
+    private void OnCollision(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isonGround = true;
+        }
+    }
+
 }
